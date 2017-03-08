78a86253ec29e3f200aee9224ca469ac20a538a5
Randall Crame <randallcrame@zipcoders-MacBook-Pro.local>
2017-03-08 15:00:09 -0500

    working on service calls

--- TeamSquadCheckers/src/services/move-service.ts
+++ TeamSquadCheckers/src/services/move-service.ts
@@ -5,16 +5,18 @@ import { Http } from '@angular/http';
 export class MoveService {
     private data: any;
     private responseData: any;
-    private url: string = "127.0.0.1:8080/generateInitialBoardState";
+    private url: string = "http://127.0.0.1:8080/playerMove";
 
     constructor(private http: Http){}
 
     submitMove(firstCoordinate: string, secondCoordinate: string) {
        console.log(this.url);
-        this.data = {
+        this.data = JSON.stringify({
             "firstCoordinate": firstCoordinate,
             "secondCoordinate": secondCoordinate
-        };
+        });
+        console.log('Before post');
+
         this.http.post(this.url, this.data).subscribe(response => {
             this.responseData = response.json();
             console.log(this.responseData);
